8ad9c1800ad60f167b8002d53da8296ff59a2705
wetooa <derpykidyt@gmail.com>
2025-03-18 14:25:35 +0800

    added solution for s23 activity

--- s23/discussion/index.js
+++ s23/discussion/index.js
@@ -4,3 +4,25 @@ let interestRate = 0.1;
 let interest = principal * interestRate;
 
 console.log(`The interest rate on your savings account is: ${interest}`);
+
+const fullName = ["Juan", "Dela", "Cruz"];
+
+let currentDate = [2025, 3, 18];
+
+const [year, month, day] = currentDate;
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+console.log(`Today is ${months[month - 1]} ${day}, ${year}`);
