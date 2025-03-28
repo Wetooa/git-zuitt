b7bdd242629390f677355bec862159c868ee518c
wetooa <derpykidyt@gmail.com>
2025-03-28 23:42:45 +0800

    added solution for s26 activity

--- s26/activity/index.js
+++ s26/activity/index.js
@@ -1,1 +1,33 @@
+// Grab the elements
+const container = document.getElementById("container");
+const text = document.getElementById("text");
 
+const themeDarkButton = document.getElementById("themeDark");
+const themeRedButton = document.getElementById("themeRed");
+const themeGreenButton = document.getElementById("themeGreen");
+const themeResetButton = document.getElementById("themeReset");
+
+// Add event listeners to buttons
+themeDarkButton.addEventListener("click", () => {
+  container.style.backgroundColor = "black";
+  text.style.color = "white";
+  text.style.fontFamily = "Courier New, monospace";
+});
+
+themeRedButton.addEventListener("click", () => {
+  container.style.backgroundColor = "red";
+  text.style.color = "yellow";
+  text.style.fontFamily = "Georgia, serif";
+});
+
+themeGreenButton.addEventListener("click", () => {
+  container.style.backgroundColor = "green";
+  text.style.color = "white";
+  text.style.fontFamily = "Verdana, sans-serif";
+});
+
+themeResetButton.addEventListener("click", () => {
+  container.style.backgroundColor = "white";
+  text.style.color = "black";
+  text.style.fontFamily = "Arial, sans-serif";
+});
